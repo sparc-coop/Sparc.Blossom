7951ca5c5154fa0766932961cb06cbfac0dbf4ee
Christine Antonio <christinejm.antonio@gmail.com>
2022-11-08 16:57:33 -0800

    initial commit

---
+++ Sparc.Blossom.Server/Program.cs
@@ -0,0 +1,26 @@
+using Microsoft.AspNetCore.Hosting.Server.Features;
+using Microsoft.AspNetCore.Hosting.Server;
+using Sparc.Blossom.Web;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddRazorPages();
+
+builder.Services.AddSingleton(sp =>
+{
+    // Get the address that the app is currently running at
+    var server = sp.GetRequiredService<IServer>();
+    var addressFeature = server.Features.Get<IServerAddressesFeature>();
+    var baseAddress = addressFeature!.Addresses.First();
+    return new HttpClient { BaseAddress = new Uri(baseAddress) };
+});
+
+var app = builder.Build();
+
+//app.UseBlazorFrameworkFiles();
+app.UseStaticFiles();
+app.MapRazorPages();
+app.MapControllers();
+app.MapFallbackToPage("/_Host");
+
+app.Run();
