3fd3514e0f1445a1f64e8c0f207f8b39c285bc75
Joseph Young <joseph@josephayoung.com>
2022-12-13 15:54:05 -0600

    Upgrade to Sparc.Blossom 7.1, add localization, fix namespaces

--- Sparc.Blossom.Server/Program.cs
+++ Sparc.Blossom.Server/Program.cs
@@ -1,29 +1,19 @@
+using Sparc.Blossom;
 using Sparc.Ibis;
-using Sparc.Kernel;
-using System.Globalization;
-
 
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddRazorPages();
 builder.Services.AddServerSideBlazor();
 
-builder.AddSparcKernel();
-builder.Services
-    .AddIbis();
+builder.AddBlossom();
+builder.Services.AddIbis();
+builder.Services.AddLocalization();
 
 var app = builder.Build();
 
-var supportedCultures = CultureInfo.GetCultures(CultureTypes.AllCultures)
-    .Select(x => x.Name)
-    .ToArray();
-
 app.UseStaticFiles();
-
-app.UseRequestLocalization(options => options
-    .AddSupportedCultures(supportedCultures)
-    .AddSupportedUICultures(supportedCultures));
-
+app.UseAllCultures();
 app.MapBlazorHub();
 app.MapFallbackToPage("/_Host");
 
