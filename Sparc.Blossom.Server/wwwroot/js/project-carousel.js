0adcac19eda78f2b2be340de196e2e488f5d72ad
Christine Antonio <christinejm.antonio@gmail.com>
2023-07-10 15:49:03 -0700

    12 col grid for desktop, navbar background blur, carousel animation for logos and projects

---
+++ Sparc.Blossom.Server/wwwroot/js/project-carousel.js
@@ -0,0 +1,7 @@
+﻿document.getElementById("carousel-backward").onmouseover = function () {
+    document.getElementById("project-wrapper").style.animation = "scrollReverse 10s linear infinite !important";
+}
+
+document.getElementById("carousel-forward").onmouseover = function () {
+    document.getElementById("project-wrapper").style.animation = "scroll 10s linear infinite !important";
+}
