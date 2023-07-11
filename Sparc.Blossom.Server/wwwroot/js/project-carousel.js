a851b3b7911bfec6847f0136520008d986a8df49
Joseph Young <joseph@josephayoung.com>
2023-07-11 08:13:17 -0500

    Blazorify JS calls

--- Sparc.Blossom.Server/wwwroot/js/project-carousel.js
+++
@@ -1,7 +0,0 @@
-﻿document.getElementById("carousel-backward").onmouseover = function () {
-    document.getElementById("project-wrapper").style.animation = "scrollReverse 10s linear infinite !important";
-}
-
-document.getElementById("carousel-forward").onmouseover = function () {
-    document.getElementById("project-wrapper").style.animation = "scroll 10s linear infinite !important";
-}
